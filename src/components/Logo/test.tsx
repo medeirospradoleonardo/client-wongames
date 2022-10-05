e1b153cf53ea3f37be2e49df38c21755700c9e12
medeirospradoleonardo <leonardo_medeiros_prado@hotmail.com>
2022-10-05 12:06:45 -0300

    148. Logo Parte 5: Criando primeiros testes

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,4 +1,5 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Logo from '.'
 
@@ -7,6 +8,17 @@ describe('<Logo />', () => {
     // renderizar o componente `render`
     // selecionar o elemento a ser testado `screen` (queries) - getByLabel...
     // expect - assertion - asserção - comparação - análise (espero que renderize a logo branca)
-    
+
+    renderWithTheme(<Logo />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#FAFAFA'
+    })
+  })
+
+  it('should render a black label when color is passed', () => {
+    renderWithTheme(<Logo color="black" />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#030517'
+    })
   })
 })
