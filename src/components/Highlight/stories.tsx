a7dd5a05b51717b7daca04590bb2e26c9bed3804
medeirospradoleonardo <leonardo_medeiros_prado@hotmail.com>
2022-10-15 13:21:46 -0300

    178. Highlight Parte 1: Apresentação do componente

---
+++ src/components/Highlight/stories.tsx
@@ -0,0 +1,9 @@
+import { Story, Meta } from '@storybook/react'
+import Highlight from '.'
+
+export default {
+  title: 'Highlight',
+  component: Highlight
+} as Meta
+
+export const Default: Story = () => <Highlight />
