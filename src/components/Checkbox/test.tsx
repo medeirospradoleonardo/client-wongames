d6ff7ee8e1ef296fe7fff1da712e6450853bf775
medeirospradoleonardo <leonardo_medeiros_prado@hotmail.com>
2022-10-19 16:29:52 -0300

    215. Checkbox Parte 2: Tornando o input dinâmico

--- src/components/Checkbox/test.tsx
+++ src/components/Checkbox/test.tsx
@@ -1,13 +1,20 @@
 import { render, screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Checkbox from '.'
 
 describe('<Checkbox />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Checkbox />)
+  it('should render with label', () => {
+    renderWithTheme(<Checkbox label="checkbox label" labelFor="check" />)
 
-    expect(screen.getByRole('heading', { name: /Checkbox/i })).toBeInTheDocument()
 
-    expect(container.firstChild).toMatchSnapshot()
+    // input a partir do papel / role
+    expect(screen.getByRole('checkbox')).toBeInTheDocument()
+
+    // input a partir da label associada
+    expect(screen.getByLabelText(/checkbox label/i)).toBeInTheDocument()
+
+    // label a partir do texto dela
+    expect(screen.getByText(/checkbox label/i)).toHaveAttribute('for', 'check')
   })
 })
