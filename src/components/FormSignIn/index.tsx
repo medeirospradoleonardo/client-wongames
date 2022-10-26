986a6f7cc553c51d386186ac3891ccd7041019fd
medeirospradoleonardo <leonardo_medeiros_prado@hotmail.com>
2022-10-26 14:51:20 -0300

    240. Refatorando para usar um componente de estilos para os Forms

--- src/components/FormSignIn/index.tsx
+++ src/components/FormSignIn/index.tsx
@@ -3,11 +3,12 @@ import { Email, Lock } from '@styled-icons/material-outlined'
 
 import Button from 'components/Button'
 import TextField from 'components/TextField'
+import { FormWrapper, FormLink } from 'components/Form'
 
 import * as S from './styles'
 
 const FormSignIn = () => (
-  <S.Wrapper>
+  <FormWrapper>
     <form>
       <TextField
         name="email"
@@ -27,14 +28,14 @@ const FormSignIn = () => (
         Sign in now
       </Button>
 
-      <S.FormLink>
+      <FormLink>
         Don’t have an account?{' '}
         <Link href="/sign-up">
           <a>Sign up</a>
         </Link>
-      </S.FormLink>
+      </FormLink>
     </form>
-  </S.Wrapper>
+  </FormWrapper>
 )
 
 export default FormSignIn
