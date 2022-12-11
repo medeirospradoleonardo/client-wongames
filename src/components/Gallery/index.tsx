83104792c0fe6103f0de0894772c1ad561b41754
medeirospradoleonardo <leonardo_medeiros_prado@hotmail.com>
2022-12-11 14:50:02 -0300

    265. Gallery Parte 5: Criando método de close com a tecla ESC (useEffect hook)

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -3,7 +3,7 @@ import { ArrowBackIos as ArrowLeft } from '@styled-icons/material-outlined/Arrow
 import { ArrowForwardIos as ArrowRight } from '@styled-icons/material-outlined/ArrowForwardIos'
 
 import Slider, { SliderSettings } from 'components/Slider'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import * as S from './styles'
 
 
@@ -54,6 +54,17 @@ export type GalleryProps = {
 const GallerySlider = ({ items }: GalleryProps) => {
 
   const [isOpen, setIsOpen] = useState(false)
+
+  useEffect(() => {
+    const handleKeyUp = ({ key }: KeyboardEvent) => {
+      key === 'Escape' && setIsOpen(false)
+    }
+
+    window.addEventListener('keyup', handleKeyUp)
+    return () => {
+      window.removeEventListener('keyup', handleKeyUp)
+    }
+  }, [])
 
   return (
     <S.Wrapper>
