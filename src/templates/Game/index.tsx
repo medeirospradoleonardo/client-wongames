9a3edabc6e049ef98f940ceab24df080f0230ae2
medeirospradoleonardo <leonardo_medeiros_prado@hotmail.com>
2022-12-16 16:05:35 -0300

    274. Adicionando a Gallery

--- src/templates/Game/index.tsx
+++ src/templates/Game/index.tsx
@@ -3,13 +3,15 @@ import Base from 'templates/Base'
 import GameInfo, { GameInfoProps } from 'components/GameInfo'
 
 import * as S from './styles'
+import Gallery, { GalleryImageProps } from 'components/Gallery'
 
 export type GameTemplateProps = {
   cover: string
-  gameInfo: GameInfoProps
+  gameInfo: GameInfoProps,
+  gallery?: GalleryImageProps[]
 }
 
-const Game = ({ cover, gameInfo }: GameTemplateProps) => (
+const Game = ({ cover, gameInfo, gallery }: GameTemplateProps) => (
   <Base>
     <S.Cover
       src={cover}
@@ -21,6 +23,10 @@ const Game = ({ cover, gameInfo }: GameTemplateProps) => (
       <S.SectionGameInfo>
         <GameInfo {...gameInfo} />
       </S.SectionGameInfo>
+
+      <S.SectionGallery>
+        {!!gallery && <Gallery items={gallery} />}
+      </S.SectionGallery>
     </S.Main>
   </Base>
 )
