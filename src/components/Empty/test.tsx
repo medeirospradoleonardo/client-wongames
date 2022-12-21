c1fbc8dcb2135c8a7b5c40f5f82e52b0957d3c73
medeirospradoleonardo <leonardo_medeiros_prado@hotmail.com>
2022-12-20 21:52:04 -0300

    288. Adicionando o estado de empty na página de Wishlist e finalizando

--- src/components/Empty/test.tsx
+++ src/components/Empty/test.tsx
@@ -10,7 +10,7 @@ const props = {
 
 describe('<Empty />', () => {
   it('should render correctly', () => {
-    renderWithTheme(<Empty {...props} hasLink />)
+    const { container } = renderWithTheme(<Empty {...props} hasLink />)
 
     expect(screen.getByRole('image', { name: /a gamer in a couch playing videogame/i })).toBeInTheDocument()
 
@@ -18,6 +18,8 @@ describe('<Empty />', () => {
     expect(screen.getByText(/a simple description/i)).toBeInTheDocument()
 
     expect(screen.getByRole('link', { name: /go back to store/i })).toHaveAttribute('href', '/')
+
+    expect(container.parentElement).toMatchSnapshot()
   })
 
   it('should not render link when hasLink is not passed', () => {
--- src/templates/Wishlist/index.tsx
+++ src/templates/Wishlist/index.tsx
@@ -1,5 +1,6 @@
 import { Container } from 'components/Container'
 import { Divider } from 'components/Divider'
+import Empty from 'components/Empty'
 import GameCard, { GameCardProps } from 'components/GameCard'
 import { Grid } from 'components/Grid'
 import Heading from 'components/Heading'
@@ -13,18 +14,31 @@ export type WishlistTemplateProps = {
   recommendedHighlight: HighlightProps
 }
 
-const Wishlist = ({ games, recommendedGames, recommendedHighlight }: WishlistTemplateProps) => (
+const Wishlist = ({
+  games = [],
+  recommendedGames,
+  recommendedHighlight
+}: WishlistTemplateProps) => (
   <Base>
     <Container>
       <Heading lineLeft lineColor='secondary'>
         Wishlist
       </Heading>
 
-      <Grid>
-        {games?.map((game, index) => (
-          <GameCard key={`wishlist-${index}`} {...game} />
-        ))}
-      </Grid>
+      {games.length ? (
+        <Grid>
+          {games?.map((game, index) => (
+            <GameCard key={`wishlist-${index}`} {...game} />
+          ))}
+        </Grid>
+      ) : (
+        <Empty
+          title='Your wishlist is empty'
+          description='Games added to your wishlist will appear here'
+          hasLink
+        />
+      )}
+
 
       <Divider />
     </Container>
