4cf1cfc0867f41f332e4a7c26e7f7cbdb8aa0c4b
medeirospradoleonardo <leonardo_medeiros_prado@hotmail.com>
2022-12-23 01:22:39 -0300

    297. Cart Page Parte 1: Criando a estrutura da página

---
+++ src/templates/Cart/index.tsx
@@ -0,0 +1,51 @@
+import CartList, { CartListProps } from 'components/CartList'
+import { Container } from 'components/Container'
+import { Divider } from 'components/Divider'
+import { GameCardProps } from 'components/GameCard'
+import { HighlightProps } from 'components/Highlight'
+import Heading from 'components/Heading'
+import ShowCase from 'components/ShowCase'
+import Base from 'templates/Base'
+
+import * as S from './styles'
+import PaymentOptions, { PaymentOptionsProps } from 'components/PaymentOptions'
+
+export type CartProps = {
+  recommendedGames: GameCardProps[]
+  recommendedHighlight: HighlightProps
+} & CartListProps &
+  Pick<PaymentOptionsProps, 'cards'>
+
+const Cart = ({
+  recommendedGames,
+  recommendedHighlight,
+  items,
+  total,
+  cards
+}: CartProps) => {
+  const handlePayment = () => ({})
+
+  return (
+    <Base>
+      <Container>
+        <Heading lineLeft lineColor='secondary'>
+          My cart
+        </Heading>
+
+        <S.Content>
+          <CartList items={items} total={total} />
+          <PaymentOptions cards={cards} handlePayment={handlePayment} />
+        </S.Content>
+        <Divider />
+      </Container>
+
+      <ShowCase
+        title='You may like these games'
+        games={recommendedGames}
+        highlight={recommendedHighlight}
+      />
+    </Base>
+  )
+}
+
+export default Cart
