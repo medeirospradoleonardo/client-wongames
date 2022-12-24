f3e019d203cec6675a7c96911b1c0d31203fd2d4
medeirospradoleonardo <leonardo_medeiros_prado@hotmail.com>
2022-12-24 00:03:13 -0300

    302. ProfileMenu Parte 3: Definindo activeLink

--- src/components/ProfileMenu/index.tsx
+++ src/components/ProfileMenu/index.tsx
@@ -2,24 +2,28 @@ import { AccountCircle, CreditCard, ExitToApp, FormatListBulleted } from '@style
 import Link from 'next/link'
 import * as S from './styles'
 
-const ProfileMenu = () => (
+export type ProfileMenuProps = {
+  activeLink?: '/profile/me' | '/profile/cards' | '/profile/orders' | string
+}
+
+const ProfileMenu = ({ activeLink }: ProfileMenuProps) => (
   <S.Nav>
-    <Link href='profile/me' passHref>
-      <S.Link>
+    <Link href='/profile/me' passHref>
+      <S.Link isActive={activeLink === '/profile/me'} title='My profile'>
         <AccountCircle size={24} />
         <span>My profile</span>
       </S.Link>
     </Link>
 
-    <Link href='profile/cards' passHref>
-      <S.Link>
+    <Link href='/profile/cards' passHref>
+      <S.Link isActive={activeLink === '/profile/cards'} title='My cards'>
         <CreditCard size={24} />
         <span>My cards</span>
       </S.Link>
     </Link>
 
-    <Link href='profile/orders' passHref>
-      <S.Link>
+    <Link href='/profile/orders' passHref>
+      <S.Link isActive={activeLink === '/profile/orders'} title='My orders'>
         <FormatListBulleted size={24} />
         <span>My orders</span>
       </S.Link>
