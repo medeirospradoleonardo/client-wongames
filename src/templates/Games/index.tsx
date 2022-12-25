15397e48cc88d0f9168075dc47e7732a427672f6
medeirospradoleonardo <leonardo_medeiros_prado@hotmail.com>
2022-12-25 18:30:21 -0300

    318. Página Games/Explore Parte 2: adicionando componentes e dados

--- src/templates/Games/index.tsx
+++ src/templates/Games/index.tsx
@@ -1,14 +1,39 @@
-import { GameCardProps } from 'components/GameCard'
+import Base from 'templates/Base'
+
+import ExploreSidebar, { ItemProps } from 'components/ExploreSidebar'
+import GameCard, { GameCardProps } from 'components/GameCard'
+import { Grid } from 'components/Grid'
+
 import * as S from './styles'
+import { KeyboardArrowDown as ArrowDown } from '@styled-icons/material-outlined'
 
 export type GamesTemplateProps = {
   games?: GameCardProps[]
+  filterItems: ItemProps[]
 }
 
-const GamesTemplate = ({ games = [] }: GamesTemplateProps) => (
-  <S.Wrapper>
-    <h1>Games</h1>
-  </S.Wrapper>
+const GamesTemplate = ({ filterItems, games = [] }: GamesTemplateProps) => (
+  <Base>
+    <S.Main>
+      <ExploreSidebar
+        items={filterItems}
+        onFilter={() => console.log('filter')}
+      />
+
+      <section>
+        <Grid>
+          {games.map((item) => (
+            <GameCard key={item.title} {...item} />
+          ))}
+        </Grid>
+
+        <S.ShowMore role='button' onClick={() => console.log('show more')}>
+          <p>Show More</p>
+          <ArrowDown size={35} />
+        </S.ShowMore>
+      </section>
+    </S.Main>
+  </Base>
 )
 
 export default GamesTemplate
