2309d7e49ccba2d92adee8f634f79b3f4d4412d0
medeirospradoleonardo <leonardo_medeiros_prado@hotmail.com>
2022-12-25 01:44:38 -0300

    315. ExploreSidebar Parte 4: Criando método de filter

--- src/components/ExploreSidebar/index.tsx
+++ src/components/ExploreSidebar/index.tsx
@@ -25,10 +25,19 @@ type Values = {
 export type ExploreSidebarProps = {
   items: ItemProps[]
   initialValues?: Values
+  onFilter: (values: Values) => void
 }
 
-const ExploreSidebar = ({ items, initialValues = {} }: ExploreSidebarProps) => {
+const ExploreSidebar = ({ items, onFilter, initialValues = {} }: ExploreSidebarProps) => {
   const [values, setValues] = useState(initialValues)
+
+  const handleChange = (name: string, value: string | boolean) => {
+    setValues((s) => ({ ...s, [name]: value }))
+  }
+
+  const handleFilter = () => {
+    onFilter(values)
+  }
 
   return (
     <S.Wrapper>
@@ -47,6 +56,7 @@ const ExploreSidebar = ({ items, initialValues = {} }: ExploreSidebarProps) => {
                 label={field.label}
                 labelFor={field.name}
                 isChecked={!!values[field.name]}
+                onCheck={(v) => handleChange(field.name, v)}
               />
             ))
           )}
@@ -61,13 +71,16 @@ const ExploreSidebar = ({ items, initialValues = {} }: ExploreSidebarProps) => {
                 label={field.label}
                 labelFor={field.name}
                 defaultChecked={field.name === values[item.name]}
+                onChange={() => handleChange(item.name, field.name)}
               />
             ))
           )}
         </div>
       ))}
 
-      <Button fullWidth size='medium'>Filter</Button>
+      <Button fullWidth size='medium' onClick={handleFilter} >
+        Filter
+      </Button>
     </S.Wrapper>
   )
 }
