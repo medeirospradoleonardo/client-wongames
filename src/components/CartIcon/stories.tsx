2f0080da9a7adf486e2e371f6c14f432089e05c8
medeirospradoleonardo <leonardo_medeiros_prado@hotmail.com>
2022-12-25 21:17:12 -0300

    320. Criando CartIcon component

---
+++ src/components/CartIcon/stories.tsx
@@ -0,0 +1,19 @@
+import { Story, Meta } from '@storybook/react'
+import CartIcon, { CartIconProps } from '.'
+
+export default {
+  title: 'CartIcon',
+  component: CartIcon,
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    }
+  }
+} as Meta
+
+export const Default: Story = () => <CartIcon />
+export const withItems: Story<CartIconProps> = (args) => <CartIcon {...args} />
+
+withItems.args = {
+  quantity: 3
+}
