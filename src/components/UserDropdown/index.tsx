f658784da0bfc51505f749cb700b58447f371312
medeirospradoleonardo <leonardo_medeiros_prado@hotmail.com>
2022-12-26 01:20:36 -0300

    328. UserDropdown Parte 1: criando o componente e estilos

---
+++ src/components/UserDropdown/index.tsx
@@ -0,0 +1,50 @@
+import {
+  AccountCircle,
+  FavoriteBorder,
+  ExitToApp
+} from '@styled-icons/material-outlined'
+import { ChevronDown } from '@styled-icons/boxicons-regular/ChevronDown'
+
+import Dropdown from 'components/Dropdown'
+
+import * as S from './styles'
+import Link from 'next/link'
+
+export type UserDropdownProps = {
+  username: string
+}
+
+const UserDropdown = ({ username }: UserDropdownProps) => (
+  <Dropdown title={
+    <>
+      <AccountCircle size={24} />
+      <S.Username>{username}</S.Username>
+      <ChevronDown size={24} />
+    </>
+  }>
+    <S.Nav>
+      <Link href='/profile/me' passHref>
+        <S.Link>
+          <AccountCircle />
+          <span>My profile</span>
+        </S.Link>
+      </Link>
+
+      <Link href='/wishlist' passHref>
+        <S.Link>
+          <FavoriteBorder />
+          <span>Wishlist</span>
+        </S.Link>
+      </Link>
+
+      <Link href='/logout' passHref>
+        <S.Link>
+          <ExitToApp />
+          <span>Logout</span>
+        </S.Link>
+      </Link>
+    </S.Nav>
+  </Dropdown>
+)
+
+export default UserDropdown
