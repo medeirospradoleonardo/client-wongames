215af97f8a750b0532de3f855ed2181cc8aec009
medeirospradoleonardo <leonardo_medeiros_prado@hotmail.com>
2022-12-27 02:21:48 -0300

    340. Populando a página de Games com dados via getStaticProps (SSG) com revalidate

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -6,17 +6,17 @@ import highlightMock from 'components/Highlight/mock'
 import cardsMock from 'components/PaymentOptions/mock'
 
 export default function CartPage(props: CartProps) {
-    return <Cart {...props} />
+  return <Cart {...props} />
 }
 
 export async function getServerSideProps() {
-    return {
-        props: {
-            items: itemsMock,
-            total: 'R$ 430,00',
-            cards: cardsMock,
-            recommendedGames: gamesMock,
-            recommendedHighlight: highlightMock
-        }
+  return {
+    props: {
+      items: itemsMock,
+      total: 'R$ 430,00',
+      cards: cardsMock,
+      recommendedGames: gamesMock,
+      recommendedHighlight: highlightMock
     }
-}   
+  }
+}
--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -1,17 +1,47 @@
 import GamesTemplate, { GamesTemplateProps } from 'templates/Games'
 
 import filterItemsMock from 'components/ExploreSidebar/mock'
-import gamesMock from 'components/GameCardSlider/mock'
+import { initializeApollo } from 'utils/apollo'
+import { gql } from '@apollo/client'
 
 export default function GamesPage(props: GamesTemplateProps) {
-    return <GamesTemplate {...props} />
+  return <GamesTemplate {...props} />
 }
 
-export async function getServerSideProps() {
-    return {
-        props: {
-            games: gamesMock,
-            filterItems: filterItemsMock
+export async function getStaticProps() {
+  const apolloClient = initializeApollo()
+
+  const { data } = await apolloClient.query({
+    query: gql`
+      query QueryGames {
+        games {
+          name
+          slug
+          cover {
+            url
+          }
+          developers {
+            name
+          }
+          price
         }
+      }
+    `
+  })
+
+  return {
+    props: {
+      revalidate: 60,
+      games: data.games.map((game) => ({
+        title: game.name,
+        developer: game.developers[0].name,
+        img: `http://localhost:1337${game.cover.url}`,
+        price: new Intl.NumberFormat('en', {
+          style: 'currency',
+          currency: 'USD'
+        }).format(game.price)
+      })),
+      filterItems: filterItemsMock
     }
+  }
 }
