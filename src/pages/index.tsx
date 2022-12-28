6e35ff42ee586aa056ee5c5f9fe942ffcb608b30
medeirospradoleonardo <leonardo_medeiros_prado@hotmail.com>
2022-12-28 16:27:41 -0300

    353. Buscando os dados do Banner no client (Next)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,9 @@ import Home, { HomeTemplateProps } from 'templates/Home'
 import bannersMock from 'components/BannerSlider/mock'
 import gamesMock from 'components/GameCardSlider/mock'
 import highlightMock from 'components/Highlight/mock'
+import { initializeApollo } from 'utils/apollo'
+import { QueryHome } from 'graphql/generated/QueryHome'
+import { QUERY_HOME } from 'graphql/queries/home'
 
 export default function Index(props: HomeTemplateProps) {
   return <Home {...props} />
@@ -13,15 +16,30 @@ export default function Index(props: HomeTemplateProps) {
 // getStaticProps => gerar estatico em build time
 // getServerSideProps => gerar via ssr a cada request
 // getInitialProps => gerar via ssr a cada request
-export async function getServerSideProps() {
+export async function getStaticProps() {
   // faz a logica
   // pode ser buscar dados numa API
   // fazer calculos/leitura de context
 
-  // retorno dos dados
+  const apolloClient = initializeApollo()
+
+  const { data } = await apolloClient.query<QueryHome>({ query: QUERY_HOME })
+
   return {
     props: {
-      banners: bannersMock,
+      revalidate: 10,
+      banners: data.banners.map((banner) => ({
+        img: `http://localhost:1337${banner.image?.url}`,
+        title: banner.title,
+        subtitle: banner.subtitle,
+        buttonLabel: banner.button?.label,
+        buttonLink: banner.button?.link,
+        ...(banner.ribbon && {
+          ribbon: banner.ribbon.text,
+          ribbonColor: banner.ribbon.color,
+          ribbonSizes: banner.ribbon.size
+        })
+      })),
       newGames: gamesMock,
       mostPopularHighlight: highlightMock,
       mostPopularGames: gamesMock,
