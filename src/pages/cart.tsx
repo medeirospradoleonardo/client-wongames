e2da4da27d4b075f9dc923cf1f564b35bd306c3d
medeirospradoleonardo <leonardo_medeiros_prado@hotmail.com>
2023-01-01 02:12:58 -0300

    369. Adicionando recommended games no frontend na página de Cart

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,22 +1,33 @@
 import Cart, { CartProps } from 'templates/Cart'
 
 import itemsMock from 'components/CartList/mock'
-import gamesMock from 'components/GameCardSlider/mock'
-import highlightMock from 'components/Highlight/mock'
 import cardsMock from 'components/PaymentOptions/mock'
+import { QUERY_RECOMMENDED } from 'graphql/queries/recommended'
+import { QueryRecommended } from 'graphql/generated/QueryRecommended'
+import { initializeApollo } from 'utils/apollo'
+import { gamesMapper, highlightMapper } from 'utils/mappers'
 
 export default function CartPage(props: CartProps) {
   return <Cart {...props} />
 }
 
 export async function getServerSideProps() {
+  const apolloClient = initializeApollo()
+
+  const { data } = await apolloClient.query<QueryRecommended>({
+    query: QUERY_RECOMMENDED
+  })
+
   return {
     props: {
       items: itemsMock,
       total: 'R$ 430,00',
       cards: cardsMock,
-      recommendedGames: gamesMock,
-      recommendedHighlight: highlightMock
+      recommendedTitle: data.recommended?.section?.title,
+      recommendedGames: gamesMapper(data.recommended?.section?.games),
+      recommendedHighlight: highlightMapper(
+        data.recommended?.section?.highlight
+      )
     }
   }
 }
