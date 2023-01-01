ab20048c59e07486cea22edefdec2ba98da4bb9f
medeirospradoleonardo <leonardo_medeiros_prado@hotmail.com>
2023-01-01 02:09:37 -0300

    368. Adicionando recommended games no frontend na página de Wishlist

--- src/templates/Wishlist/index.tsx
+++ src/templates/Wishlist/index.tsx
@@ -10,12 +10,14 @@ import Base from 'templates/Base'
 
 export type WishlistTemplateProps = {
   games?: GameCardProps[]
+  recommendedTitle?: string
   recommendedGames: GameCardProps[]
   recommendedHighlight: HighlightProps
 }
 
 const Wishlist = ({
   games = [],
+  recommendedTitle,
   recommendedGames,
   recommendedHighlight
 }: WishlistTemplateProps) => (
@@ -42,7 +44,7 @@ const Wishlist = ({
       <Divider />
     </Container>
     <ShowCase
-      title="You may like these games"
+      title={recommendedTitle || 'You may like these games'}
       games={recommendedGames}
       highlight={recommendedHighlight}
     />
