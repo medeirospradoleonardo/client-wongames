4caee28d8a54869678aa1b0a25ddb3aacd2712ff
medeirospradoleonardo <leonardo_medeiros_prado@hotmail.com>
2023-01-25 17:56:39 -0300

    449. Criando a página /reset-password and FormResetPassword

---
+++ src/components/FormResetPassword/index.tsx
@@ -0,0 +1,87 @@
+import { useState } from 'react'
+import { useRouter } from 'next/router'
+import { signIn } from 'next-auth/client'
+import { Lock, ErrorOutline } from '@styled-icons/material-outlined'
+
+import Button from 'components/Button'
+import TextField from 'components/TextField'
+import { FormWrapper, FormLoading, FormError } from 'components/Form'
+
+import { FieldErrors } from 'utils/validation'
+
+const FormResetPassword = () => {
+  const [formError, setFormError] = useState('')
+  const [fieldError, setFieldError] = useState<FieldErrors>({})
+  const [values, setValues] = useState({ password: '', confirmPassword: '' })
+  const [loading, setLoading] = useState(false)
+  const routes = useRouter()
+  const { push, query } = routes
+
+  const handleInput = (field: string, value: string) => {
+    setValues((s) => ({ ...s, [field]: value }))
+  }
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault()
+    setLoading(true)
+
+    const errors = {} // validate after
+
+    if (Object.keys(errors).length) {
+      setFieldError(errors)
+      setLoading(false)
+      return
+    }
+
+    setFieldError({})
+
+    // sign in
+    const result = await signIn('credentials', {
+      ...values,
+      redirect: false,
+      callbackUrl: `${window.location.origin}${query?.callbackUrl || ''}`
+    })
+
+    if (result?.url) {
+      return push(result?.url)
+    }
+
+    setLoading(false)
+
+    // jogar o erro
+    setFormError('username or password is invalid')
+  }
+
+  return (
+    <FormWrapper>
+      {!!formError && (
+        <FormError>
+          <ErrorOutline /> {formError}
+        </FormError>
+      )}
+      <form onSubmit={handleSubmit}>
+        <TextField
+          name="password"
+          placeholder="Password"
+          type="password"
+          error={fieldError?.password}
+          onInputChange={(v) => handleInput('password', v)}
+          icon={<Lock />}
+        />
+        <TextField
+          name="confirm_password"
+          placeholder="Confirm password"
+          type="password"
+          error={fieldError?.confirm_password}
+          onInputChange={(v) => handleInput('confirm_password', v)}
+          icon={<Lock />}
+        />
+        <Button type="submit" size="large" fullWidth disabled={loading}>
+          {loading ? <FormLoading /> : <span>Reset password</span>}
+        </Button>
+      </form>
+    </FormWrapper>
+  )
+}
+
+export default FormResetPassword
