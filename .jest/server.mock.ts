c3484d8f06a3c235c750df6c338bfbaaf9fc2778
medeirospradoleonardo <leonardo_medeiros_prado@hotmail.com>
2023-02-08 15:18:27 -0300

    455. Configurando o Mock Service Worker (MSW) para testar APIs

---
+++ .jest/server.mock.ts
@@ -0,0 +1,16 @@
+import { server } from '../src/utils/mockServer/server'
+
+beforeAll(() => {
+  // ficar escutando todas as chamadas nos testes
+  server.listen()
+})
+
+afterEach(() => {
+  // reseta todos os handlers para caso eles sejam chamados novamente
+  server.resetHandlers()
+})
+
+afterAll(() => {
+  // fecha o server limpa os testes
+  server.close()
+})
