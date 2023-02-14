617111e636399843035098c71034d00098828a0a
medeirospradoleonardo <leonardo_medeiros_prado@hotmail.com>
2023-02-13 21:57:53 -0300

    475. Criando método de removeFromWishlist com TDD

--- src/hooks/use-wishlist/index.tsx
+++ src/hooks/use-wishlist/index.tsx
@@ -62,7 +62,7 @@ const WishlistProvider = ({ children }: WishlistProviderProps) => {
     }
   )
 
-  const { data, loading } = useQueryWishlist({
+  const { data, loading: loadingQuery } = useQueryWishlist({
     skip: !session?.user?.email,
     context: { session },
     variables: {
@@ -100,7 +100,16 @@ const WishlistProvider = ({ children }: WishlistProviderProps) => {
       }
     })
   }
-  const removeFromWishlist = (id: string) => { }
+  const removeFromWishlist = (id: string) => {
+    updateList({
+      variables: {
+        input: {
+          where: { id: wishlistId },
+          data: { games: wishlistIds.filter((gameId: string) => gameId !== id) }
+        }
+      }
+    })
+  }
 
   return (
     <WishlistContext.Provider
@@ -109,7 +118,7 @@ const WishlistProvider = ({ children }: WishlistProviderProps) => {
         isInWishlist,
         addToWishlist,
         removeFromWishlist,
-        loading
+        loading: loadingQuery || loadingCreate || loadingUpdate
       }}
     >
       {children}
