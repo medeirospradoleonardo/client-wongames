f76ba74ae608fdec9864859d0f27e20b80d59f24
medeirospradoleonardo <leonardo_medeiros_prado@hotmail.com>
2023-02-15 01:43:19 -0300

    502. Inicializando a integração com o Stripe

--- src/templates/Cart/index.tsx
+++ src/templates/Cart/index.tsx
@@ -8,12 +8,18 @@ import ShowCase from 'components/ShowCase'
 import Base from 'templates/Base'
 
 import * as S from './styles'
+
+import { loadStripe } from '@stripe/stripe-js'
+import { Elements } from '@stripe/react-stripe-js'
 import PaymentForm from 'components/PaymentForm'
+import { Info } from '@styled-icons/material-outlined'
 
 export type CartProps = {
   recommendedGames: GameCardProps[]
   recommendedHighlight: HighlightProps
 } & CartListProps
+
+const stripe = loadStripe(`${process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY}`)
 
 const Cart = ({ recommendedGames, recommendedHighlight }: CartProps) => {
   return (
@@ -24,9 +30,18 @@ const Cart = ({ recommendedGames, recommendedHighlight }: CartProps) => {
         </Heading>
         <S.Content>
           <CartList />
-
-          <PaymentForm />
+          <Elements stripe={stripe}>
+            <PaymentForm />
+          </Elements>
         </S.Content>
+        <S.Text>
+          <Info size={18} /> Your purchase is protected by a secure connection
+          from the WON platform. By purchasing from our store you agree and
+          agree to our <a href="#">terms of use.</a> After making the purchase
+          you are entitled to a refund within a maximum of 30 days, without any
+          additional cost, as long as the download of the purchased game has not
+          occurred after your purchase.
+        </S.Text>
         <Divider />
       </Container>
 
