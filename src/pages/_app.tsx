39df06cb107b349c99513b7dbd8716ae163cfa63
medeirospradoleonardo <leonardo_medeiros_prado@hotmail.com>
2023-02-21 22:37:35 -0300

    542. Configurando o manifest e ícones para o PWA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -33,7 +33,7 @@ function App({ Component, pageProps }: AppProps) {
                 <meta name="theme-color" content="#06092B" />
                 <meta
                   name="description"
-                  content="The best game store in the world!"
+                  content="The best Game Store in the world!"
                 />
               </Head>
               <DefaultSeo {...SEO} />
