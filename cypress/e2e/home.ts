176357f13a66184d51e447ef05c2ba28d6eb0d94
medeirospradoleonardo <leonardo_medeiros_prado@hotmail.com>
2023-02-26 23:45:13 -0300

    559. Refatorando: criando comandos customizados

--- cypress/e2e/home.ts
+++ cypress/e2e/home.ts
@@ -2,24 +2,8 @@
 
 describe('Home Page', () => {
   it('should render home sections', () => {
-    // visitar a pagina
     cy.visit('/')
 
-    cy.get('.slick-slider').within(() => {
-      cy.findByRole('heading', { name: /cyberpunk 2077/i })
-      cy.findByRole('link', { name: /buy now/i })
-
-      cy.get('.slick-dots > :nth-child(2) > button').click()
-      cy.wait(500)
-
-      cy.findByRole('heading', { name: /horizon zero dawn/i })
-      cy.findByRole('link', { name: /buy now/i })
-
-      cy.get('.slick-dots > :nth-child(3) > button').click()
-      cy.wait(500)
-
-      cy.findByRole('heading', { name: /Huge promotion!/i })
-      cy.findByRole('link', { name: /browse games/i })
-    })
+    cy.shouldRenderBanner()
   })
 })
