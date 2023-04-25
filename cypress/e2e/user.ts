8dd022dc8739b80e9e27e562175db9c0d43f6308
medeirospradoleonardo <leonardo_medeiros_prado@hotmail.com>
2023-04-25 02:14:40 -0300

    582. Fazendo o teste para o SignIn

--- cypress/e2e/user.ts
+++ cypress/e2e/user.ts
@@ -3,7 +3,7 @@
 import { createUser } from "../support/generate"
 
 describe('User', () => {
-  it('should sign up', () => {
+  it.skip('should sign up', () => {
     const user = createUser()
 
     cy.visit('/sign-up')
@@ -11,5 +11,19 @@ describe('User', () => {
 
     cy.url().should('eq', `${Cypress.config().baseUrl}/`)
     cy.findByText(user.username).should('exist')
+  })
+
+  it('should sign in and sign out', () => {
+    cy.visit('/sign-in')
+
+    cy.findByPlaceholderText(/email/i).type('e2e@wongames.com')
+    cy.findByPlaceholderText(/^password/i).type('123456')
+    cy.findByRole('button', { name: /sign in now/i }).click()
+
+    cy.findByText(/cypress/i).should('exist').click()
+    cy.findByText(/sign out/i).click()
+
+    cy.findByRole('link', { name: /sign in/i }).should('exist')
+    cy.findByText(/cypress/i).should('not.exist')
   })
 })
