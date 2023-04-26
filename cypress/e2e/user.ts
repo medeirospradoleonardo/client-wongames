a5ec4ec6dbdf3cf311f026a068c8353a4ed599ec
medeirospradoleonardo <leonardo_medeiros_prado@hotmail.com>
2023-04-26 00:44:33 -0300

    584. Testando as rotas protegidas e os redirects após sign in

--- cypress/e2e/user.ts
+++ cypress/e2e/user.ts
@@ -25,4 +25,20 @@ describe('User', () => {
     cy.findByRole('link', { name: /sign in/i }).should('exist')
     cy.findByText(/cypress/i).should('not.exist')
   })
+
+  it.only('should sign in the user and redirect to the page that it was defined previously', () => {
+    cy.visit('/profile/me')
+
+    // redirecionado para o sign-in com a callback do profile/me
+    cy.location('href').should('eq', `${Cypress.config().baseUrl}/sign-in?callbackUrl=/profile/me`)
+
+    // fazer o sign in
+    cy.signIn()
+
+    // espero ser redirecionado para o profile/me
+    cy.location('href').should('eq', `${Cypress.config().baseUrl}/profile/me`)
+
+    cy.findByLabelText(/username/i).should('have.value', 'cypress')
+    cy.findByLabelText(/e-mail/i).should('have.value', 'e2e@wongames.com')
+  })
 })
