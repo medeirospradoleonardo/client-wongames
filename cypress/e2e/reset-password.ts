dece88856d4f41064d57ea7e2aba8c34f068cce6
medeirospradoleonardo <leonardo_medeiros_prado@hotmail.com>
2023-05-02 23:45:11 -0300

    588. Utilizando o Intercept para o next-auth

--- cypress/e2e/reset-password.ts
+++ cypress/e2e/reset-password.ts
@@ -35,9 +35,47 @@ describe('Reset Password', () => {
 
     cy.findByPlaceholderText(/^password/i).type('123')
     cy.findByPlaceholderText(/confirm password/i).type('123')
-
     cy.findByRole('button', { name: /reset password/i }).click()
 
     cy.findByText(/Incorrect code provided/i).should('exist')
+  })
+
+  it('should fill the input and redirect to the home page with the user signed in', () => {
+    // interceptar as chamadas 
+
+    // rota do nosso backend
+    cy.intercept('POST', '**/auth/reset-password', {
+      statusCode: 200,
+      body: { user: { email: 'cypress@email.com' } }
+    })
+
+    // rota do credentials do next-auth
+    cy.intercept('POST', '**/auth/callback/credentials*', {
+      statusCode: 200,
+      body: { user: { email: 'cypress@email.com' } }
+    })
+
+    // rota de session do next-auth
+    cy.intercept('GET', '**/auth/session*', {
+      statusCode: 200,
+      body: { user: { name: 'cypress', email: 'cypress@email.com' } }
+    })
+
+    // usuario vai entrar na pagina de reset
+    cy.visit('/reset-password?code=valid_token')
+
+    // vai preencher as senhas (ja com o token valido)
+    cy.findByPlaceholderText(/^password/i).type('123')
+    cy.findByPlaceholderText(/confirm password/i).type('123')
+    cy.findByRole('button', { name: /reset password/i }).click()
+
+
+    // o sign in acontece no background
+
+    // redireciona para a home
+    cy.url().should('eq', `${Cypress.config().baseUrl}/`)
+
+    // tem o usuario logado com o name no menu
+    cy.findByText(/cypress/i).should('exist')
   })
 })
